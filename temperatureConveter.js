d88f55650cdc324dcf06acd0f8ff3aa31df57002
Njabulo <nnshiwu@gmail.com>
2024-01-02 20:38:03 +0200

    Added a console.log and string interpolation to log the temperature in fahrenheit to the console.

--- temperatureConveter.js
+++ temperatureConveter.js
@@ -8,3 +8,5 @@ let celsius = kelvin - 273;
 /*Code below converts celcius to fahrenhei and the value
 attained to a variable called fahrenheit*/
 let fahrenheit = Math.floor(celsius *(9/5) + 32);//Math.floor rounds off the attained value.
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);

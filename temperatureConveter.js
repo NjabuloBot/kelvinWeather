9e9385036d5f6d48eab7e644a9a56e05015ac109
Njabulo <nnshiwu@gmail.com>
2024-01-02 20:26:53 +0200

    Added a fahrenheit variable

--- temperatureConveter.js
+++ temperatureConveter.js
@@ -1,5 +1,8 @@
 //constant kelvin variable that has the value of 293
 const kelvin = 293;
+
 /*Code below converts kelvins to celcius by subtracting 273 
 and assigning to a variable named celcius*/
 let celsius = kelvin - 273;
+
+let fahrenheit = celsius *(9/5) + 32;

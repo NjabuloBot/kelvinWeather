c04ad5e6c016ad8702979510c59b29ded74abbec
Njabulo <nnshiwu@gmail.com>
2024-01-02 20:24:24 +0200

    Added a new variable called celcius and a comment that explains its code.

--- temperatureConveter.js
+++ temperatureConveter.js
@@ -1,2 +1,5 @@
 //constant kelvin variable that has the value of 293
 const kelvin = 293;
+/*Code below converts kelvins to celcius by subtracting 273 
+and assigning to a variable named celcius*/
+let celsius = kelvin - 273;
